c3bff2c7da4874a3ec5a0878ea11f01d9e094a21
Pascal Lacroix <p.lacroix@promotion-sociale.be>
2024-04-27 14:03:26 +0200

    Structure MVC initiale

---
+++ app/config/params.php
@@ -0,0 +1,8 @@
+<?php
+// PARAMS
+
+// Paramètres de connexion à la DB
+define('DB_HOST', 'localhost:8889');
+define('DB_NAME', 'photo_hunter');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
---
+++ core/init.php
@@ -0,0 +1,8 @@
+<?php
+// Initialisation
+
+// 1. Charger les Params
+require_once '../app/config/params.php';
+
+// 2. Charger la connexion
+require_once '../core/connexion.php';
